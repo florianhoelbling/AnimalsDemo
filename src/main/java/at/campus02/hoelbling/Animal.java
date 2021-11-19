26cc3317977dc1b3f23090f9905671582cf94cd7
Florian Hoelbling <s50721@edu.campus02.at>
2021-11-19 19:51:02 +0100

    getter setter created giveAnimalLoud created getNameAndAge created

--- src/main/java/at/campus02/hoelbling/Animal.java
+++ src/main/java/at/campus02/hoelbling/Animal.java
@@ -2,4 +2,60 @@ package at.campus02.hoelbling;
 
 public class Animal {
 
+    private String name;
+    private int age;
+    private String species;
+    private String animalLoud;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getSpecies() {
+        return species;
+    }
+
+    public void setSpecies(String species) {
+        this.species = species;
+    }
+
+    public String getAnimalLoud() {
+        return animalLoud;
+    }
+
+    public void setAnimalLoud(String animalLoud) {
+        this.animalLoud = animalLoud;
+    }
+
+    private String giveAnimalLoud() {
+        switch (species) {
+            case "Löwe":
+                setAnimalLoud("Roarr");
+                return getAnimalLoud();
+            case "Hund":
+                setAnimalLoud("Wuff");
+                return getAnimalLoud();
+            case "Katze":
+                setAnimalLoud("Miau");
+                return getAnimalLoud();
+            default:
+                return null;
+        }
+    }
+
+    private String getNameAndAge() {
+        return getName() + " (" + getAge() + ")";
+    }
 }
